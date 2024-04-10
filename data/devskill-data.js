fe982fb35358693a7abaa9f6b0dab3254c9ab8ee
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-04-10 18:46:31 -0400

    create devskills data

---
+++ data/devskill-data.js
@@ -0,0 +1,9 @@
+const devskills = [
+  {skill: "UX/UI", proficiency: true, _id: 125223},
+  {skill: "Front-end", proficiency: true, _id: 127904},
+  {skill: "Back-end", proficiency: false, _id: 139608},
+]
+
+export {
+  devskills
+}

11bf13b9157c291dd789322e8ea3ff65c6cad48a
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-04-10 19:31:06 -0400

    forEach method update

--- routes/devskills.js
+++ routes/devskills.js
@@ -4,7 +4,7 @@ import * as devskillsCtrl from '../controllers/devskills.js'
 
 const router = Router()
 
-// GET localhost:3000/todos
+// GET localhost:3000/devskills
 router.get('/', devskillsCtrl.index)
 
 export { router }

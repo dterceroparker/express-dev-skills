745ef2c5e3ec580d161eada022c50a8d51953482
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-04-10 20:18:20 -0400

    Built-in Types for Properties complete

--- models/devskill.js
+++ models/devskill.js
@@ -3,6 +3,14 @@ import mongoose from 'mongoose'
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 	
-const todoSchema = new Schema({
+const devskillSchema = new Schema({
   text: String,
+  done: Boolean
 })
+
+// Compile the schema into a model and export it
+const Devskill = mongoose.model('Devskill', devskillSchema)
+
+export {
+  devskillSchema
+}

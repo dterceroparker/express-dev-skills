4dc783b1281438ef217404f8efc1225ccacb5772
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-04-13 10:37:54 -0400

    edit.ejs template literals testing

--- data/devskill-data.js
+++ data/devskill-data.js
@@ -1,7 +1,7 @@
 const devskills = [
-  {skill: "UX/UI", proficiency: true, _id: 125223},
-  {skill: "Front-end", proficiency: true, _id: 127904},
-  {skill: "Back-end", proficiency: false, _id: 139608},
+  {skill: "UX/UI", proficiency: "Beginner", _id: 125223},
+  {skill: "Front-end", proficiency: "Intermediate", _id: 127904},
+  {skill: "Back-end", proficiency: "Advanced", _id: 139608},
 ]
 
 export {
